20fdb99f5ee1b46f1c6a93d5e9fd2952fae827ab
f.bernhart <f.bernhart@ambifox.com>
2024-03-13 13:53:33 +0100

    add Tasks

---
+++ 3DRendering/task1.c
@@ -0,0 +1,11 @@
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    // Normalized pixel coordinates (from 0 to 1)
+    vec2 uv = fragCoord/iResolution.xy;
+    
+    // Make it yellow
+    vec3 col = vec3(255, 255, 0);
+    
+    // Output to screen
+    fragColor = vec4(col,1.0);
+}
---
+++ cppp/pointer.cpp
@@ -0,0 +1,10 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    
+    string test = 'Test'
+    string pointer_test = &test
+
+    return 0;
+}
